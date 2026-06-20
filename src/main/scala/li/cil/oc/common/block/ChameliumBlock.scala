4a43ba8e85fc8d7683e7092dbab68629121dd5be
31415-n <lemuran2@gmail.com>
2026-06-20 16:43:01 +0700

    Fix ChameliumBlock displaying as white in inventory
    Override createItemStack to set damage value from default block state,
    ensuring the block item renders with the correct color tint.
    
    Remove dead @EventBusSubscriber onBuildCreativeTab handler — Forge
    only registers static @SubscribeEvent methods, but this was an
    instance method on a Scala class, so it was never invoked.

--- src/main/scala/li/cil/oc/common/block/ChameliumBlock.scala
+++ src/main/scala/li/cil/oc/common/block/ChameliumBlock.scala
@@ -1,6 +1,5 @@
 package li.cil.oc.common.block
 
-import li.cil.oc.{CreativeTab, OpenComputers}
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties
@@ -11,20 +10,22 @@ import net.minecraft.world.level.block.state.properties.EnumProperty
 import net.minecraft.world.level.block.state.{StateDefinition => StateContainer}
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.{BlockGetter => IBlockReader}
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent
-import net.minecraftforge.eventbus.api.SubscribeEvent
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber
 
 object ChameliumBlock {
   final val Color = EnumProperty.create("color", classOf[DyeColor])
 }
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, modid = OpenComputers.ID)
 class ChameliumBlock(props: Properties) extends SimpleBlock(props) {
   protected override def createBlockStateDefinition(builder: StateContainer.Builder[Block, BlockState]): Unit = {
     builder.add(ChameliumBlock.Color)
   }
   registerDefaultState(stateDefinition.any.setValue(ChameliumBlock.Color, DyeColor.BLACK))
+
+  override def createItemStack(amount: Int = 1): ItemStack = {
+    val stack = new ItemStack(this, amount)
+    stack.setDamageValue(defaultBlockState.getValue(ChameliumBlock.Color).getId)
+    stack
+  }
 
   override def getCloneItemStack(world: IBlockReader, pos: BlockPos, state: BlockState): ItemStack = {
     val stack = new ItemStack(this)
@@ -34,13 +35,4 @@ class ChameliumBlock(props: Properties) extends SimpleBlock(props) {
 
   override def getStateForPlacement(ctx: BlockItemUseContext): BlockState =
     defaultBlockState.setValue(ChameliumBlock.Color, DyeColor.byId(ctx.getItemInHand.getDamageValue))
-
-  @SubscribeEvent
-  def onBuildCreativeTab(e: BuildCreativeModeTabContentsEvent): Unit = {
-    if (e.getTabKey == CreativeTab.CREATIVE_TABS.getRegistryKey) {
-      val stack = new ItemStack(this, 1)
-      stack.setDamageValue(defaultBlockState.getValue(ChameliumBlock.Color).getId)
-      e.accept(stack)
-    }
-  }
 }
