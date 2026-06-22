5b0f35aa4f3c3a0da7374c956ff6f34e8dffc693
あっきー <151368291+akki697222@users.noreply.github.com>
2026-06-22 17:51:34 +0300

    fix: harvest tool via IForgeBlock.getHarvestTool extension

--- src/main/scala/li/cil/oc/util/ItemUtils.scala
+++ src/main/scala/li/cil/oc/util/ItemUtils.scala
@@ -26,6 +26,7 @@ import net.minecraft.core.{BlockPos, RegistryAccess}
 import net.minecraft.tags.BlockTags
 import net.minecraft.world.inventory.CraftingContainer
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraftforge.common.extensions.IForgeBlock
 import net.minecraftforge.registries.ForgeRegistries
 
 import scala.collection.convert.ImplicitConversionsToScala._
@@ -57,7 +58,10 @@ object ItemUtils {
   }
 
   def getHarvestTool(state: BlockState): String = {
-    val forgeTool = state.getHarvestTool
+    val forgeTool = state.getBlock match {
+      case block: IForgeBlock => block.getHarvestTool(state, null, null)
+      case _ => null
+    }
     if (forgeTool != null && forgeTool.nonEmpty) forgeTool
     else if (state.is(BlockTags.MINEABLE_WITH_PICKAXE)) "pickaxe"
     else if (state.is(BlockTags.MINEABLE_WITH_AXE)) "axe"
