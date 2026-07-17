ce9eacdc9d071769587a63767698ce6534e05a19
YakovlevDanDmi <YakovlevDanDmi@gmail.com>
2026-07-17 13:57:43 +0500

    Fix Optional<T> parameter via generic type resolution

--- src/main/scala/li/cil/oc/integration/computercraft/DriverPeripheral.java
+++ src/main/scala/li/cil/oc/integration/computercraft/DriverPeripheral.java
@@ -28,6 +28,8 @@ import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -185,6 +187,7 @@ public final class DriverPeripheral implements li.cil.oc.api.driver.DriverBlock 
 
         private Object[] buildInvokeArguments(final Method method, final Object[] args, final IComputerAccess access) {
             final Class<?>[] parameterTypes = method.getParameterTypes();
+            final Type[] genericParameterTypes = method.getGenericParameterTypes();
             final Object[] invokeArgs = new Object[parameterTypes.length];
 
             int argIndex = 0;
@@ -198,6 +201,14 @@ public final class DriverPeripheral implements li.cil.oc.api.driver.DriverBlock 
                     invokeArgs[i] = UnsupportedLuaContext.instance();
                 } else if (type == ObjectArguments.class) {
                     invokeArgs[i] = new ObjectArguments(args);
+                } else if (type == Optional.class) {
+                    final Class<?> innerType = resolveOptionalType(genericParameterTypes[i]);
+                    if (argIndex < args.length && args[argIndex] != null) {
+                        invokeArgs[i] = Optional.ofNullable(coerce(args[argIndex], innerType));
+                    } else {
+                        invokeArgs[i] = Optional.empty();
+                    }
+                    argIndex++;
                 } else {
                     invokeArgs[i] = argIndex < args.length ? coerce(args[argIndex], type) : defaultValue(type);
                     argIndex++;
@@ -205,6 +216,16 @@ public final class DriverPeripheral implements li.cil.oc.api.driver.DriverBlock 
             }
 
             return invokeArgs;
+        }
+
+        private Class<?> resolveOptionalType(Type genericType) {
+            if (genericType instanceof ParameterizedType parameterized) {
+                final Type actual = parameterized.getActualTypeArguments()[0];
+                if (actual instanceof Class<?> clazz) {
+                    return clazz;
+                }
+            }
+            return Object.class;
         }
 
         private Object coerce(final Object value, final Class<?> type) {
